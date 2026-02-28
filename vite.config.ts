6a19d80610d6258d84e37569dcf7291763c87ee8
Ahmed <AhmedBahlaoui@outlook.com>
2026-02-28 00:24:27 +0000

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/doc-to-display-star/",
+  base: "/portfolio-website/",
   server: {
     host: "::",
     port: 8080,
